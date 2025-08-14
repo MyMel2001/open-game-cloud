8a71609c560bef85c67c059845eb3c358f59e1f2
My Mel (Holder Account) <163201376+mymel2001-holder@users.noreply.github.com>
2025-08-14 14:15:28 -0500

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,9 @@
+docker run -it --rm \
+  --gpus all \
+  --net=host \
+  --device /dev/dri \
+  --device /dev/input \
+  -v /run/user/1000/pulse:/run/user/1000/pulse \
+  -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
+  -v $HOME/.config/sunshine:/home/gamer/.config/sunshine \
+  game-stream-vnc
